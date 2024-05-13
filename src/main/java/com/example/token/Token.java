5187860006fbc4bebf59f0df2636d43c9f668723
Tran cong tien <tientc.21it@vku.udn.vn>
2024-05-13 17:09:58 +0700

    #2 done

---
+++ src/main/java/com/example/token/Token.java
@@ -0,0 +1,27 @@
+package  com.example.token;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * Token (lexeme) details
+ */
+@Builder
+@Getter
+@ToString
+public class Token {
+    /**
+     * Type of the token
+     */
+    private final TokenType type;
+    /**
+     * Value of the token
+     */
+    private final String value;
+    /**
+     * Row number where the token is defined
+     */
+    private final Integer rowNumber;
+}
+
